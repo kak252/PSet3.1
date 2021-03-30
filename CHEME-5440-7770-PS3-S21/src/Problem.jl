138ee7ee36c98e71a19f970ace486634e3d7c3fb
kak252 <79181115+kak252@users.noreply.github.com>
2021-03-30 10:13:46 -0400

    Add files via upload

---
+++ CHEME-5440-7770-PS3-S21/src/Problem.jl
@@ -0,0 +1,95 @@
+# ----------------------------------------------------------------------------------- #
+# Copyright (c) 2018 Varnerlab
+# Robert Frederick Smith School of Chemical and Biomolecular Engineering
+# Cornell University, Ithaca NY 14850
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# ----------------------------------------------------------------------------------- #
+
+# ----------------------------------------------------------------------------------- #
+# Function: generate_problem_dictionary
+# Description: Holds simulation and model parameters as key => value pairs in a Julia Dict()
+# Generated on: 2018-03-15T00:00:56.939
+#
+# Output arguments:
+# data_dictionary::Dict{AbstractString,Any} => Dictionary holding model and simulation parameters as key => value pairs
+# ----------------------------------------------------------------------------------- #
+function generate_problem_dictionary()
+
+	# Load the stoichiometric network from disk -
+	path_to_network_file = joinpath(_PATH_TO_CONFIG,"Network.dat")
+	stoichiometric_matrix = readdlm(path_to_network_file);
+	#stoichiometric_matrix = Array{Float64,2}(stoichiometric_matrix)
+
+	# What is the system dimension? -
+	(number_of_species,number_of_reactions) = size(stoichiometric_matrix)
+
+	# Setup the flux bounds array -
+	flux_bounds_array = zeros(number_of_reactions,2)
+	# TODO: update the flux_bounds_array for each reaction in your network
+	# TODO: col 1 => lower bound
+	# TODO: col 2 => upper bound
+	# TODO: each row is a reaction
+	#kcat_2133=88.1 #kcat units are s-1
+	#kcat_6345=203
+	#kcat_4321=34.5
+	#kcat_3531=249
+	#kcat_1141339=13.7
+	#enzyme_conc=0.01	#umolg/DW
+	#vmax = kcat*enzyme_conc
+
+	path_to_flux_file = joinpath(_PATH_TO_CONFIG,"flux_bounds_array.dat")
+	flux_bounds_array = readdlm(path_to_flux_file);
+	#flux_bounds_array = Array{Float64,2}(flux_bounds_array);
+
+
+
+	# for i in 1:number_of_reactions
+	#	flux_bounds_array[i,1] = parse(Float64,flux_bounds_array[i,1]);
+	#	flux_bounds_array[i,2] = parse(Float64,flux_bounds_array[i,2]);
+	# end
+	
+	# Setup default species bounds array -
+	species_bounds_array = zeros(number_of_species,2)
+	# TODO: NOTE - we by default assume Sv = 0, so species_bounds_array should be a M x 2 array of zeros
+	# TODO: however, if you formulate the problem differently you *may* need to change this 
+
+	# Min/Max flag - default is minimum -
+	is_minimum_flag = true
+
+	# Setup the objective coefficient array -
+	objective_coefficient_array = zeros(number_of_reactions)
+	# TODO: update me to maximize Urea production (Urea leaving the virtual box) 
+	# TODO: if is_minimum_flag = true => put a -1 in the index for Urea export
+		#path_to_coefficient_file = joinpath(_PATH_TO_CONFIG,"objective_coefficient_array.dat")
+		#objective_coefficient_array[:,1] = readdlm(path_to_coefficient_file);
+	objective_coefficient_array[13]=-1		#13 is the index for urea in the stoichiometric_matrix
+	
+	# =============================== DO NOT EDIT BELOW THIS LINE ============================== #
+	data_dictionary = Dict{String,Any}()
+	data_dictionary["stoichiometric_matrix"] = stoichiometric_matrix
+	data_dictionary["objective_coefficient_array"] = objective_coefficient_array
+	data_dictionary["flux_bounds_array"] = flux_bounds_array;
+	data_dictionary["species_bounds_array"] = species_bounds_array
+	data_dictionary["is_minimum_flag"] = is_minimum_flag
+	data_dictionary["number_of_species"] = number_of_species
+	data_dictionary["number_of_reactions"] = number_of_reactions
+	# =============================== DO NOT EDIT ABOVE THIS LINE ============================== #
+	return data_dictionary
+end
